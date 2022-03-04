aef59df42168569016f6efb872516778c3db61e9
MMarussi <jmmarussi32@gmail.com>
2022-03-04 17:56:07 -0300

    Initial commit

---
+++ src/components/Routes/index.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes as RoutesSwitch, Route, Navigate } from 'react-router-dom';
+
+import { useSelector } from 'contexts/UserContext';
+
+import Suspense from '../Suspense';
+
+import { ROUTES } from './constants';
+import styles from './styles.module.scss';
+
+function Routes() {
+  const user = useSelector((state) => state.user);
+
+  return (
+    <Router>
+      <div className={styles.container}>
+        <Suspense>
+          <RoutesSwitch>
+            {ROUTES.map(({ redirectTo, path, element, ...config }) => (
+              <Route
+                key={path}
+                path={path}
+                element={redirectTo?.(user) ? <Navigate to={redirectTo?.(user) as string} /> : element}
+                {...config}
+              />
+            ))}
+          </RoutesSwitch>
+        </Suspense>
+      </div>
+    </Router>
+  );
+}
+
+export default Routes;
